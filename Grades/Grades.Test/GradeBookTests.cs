88490fc1557b35e2d4579d17dca0f3a76706efa0
EUR-DOMAIN1\25599 <joshua.robinson@aruplab.com>
2018-11-02 12:51:54 -0600

    Stuff

--- Grades/Grades.Test/GradeBookTests.cs
+++ Grades/Grades.Test/GradeBookTests.cs
@@ -32,5 +32,42 @@ namespace Grades.Test
             GradeStatistics result = book.ComputeStatistics();
             Assert.AreEqual(10, result.LowestGrade);
         }
+
+        [TestMethod]
+
+        public void UppercaseString()
+        {
+            string name = "scott";
+            name = name.ToUpper();
+
+            Assert.AreEqual("SCOTT", name);
+        }
+
+        [TestMethod]
+
+        public void AddDaysToDateTime()
+        {
+            DateTime time = new DateTime(2018, 1, 1);
+            time = time.AddDays(1);
+
+            Assert.AreEqual(2, time.Day);
+        }
+
+        [TestMethod]
+
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
     }
 }
